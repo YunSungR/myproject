d7a37425e8ce47200cac503ca367957ac490e759
sryun <tjdfuf27@naver.com>
2024-04-24 22:19:29 +0900

    react init

--- src/main/java/com/myproject/myproject/controller/IndexController.java
+++ src/main/java/com/myproject/myproject/controller/IndexController.java
@@ -3,13 +3,27 @@ package com.myproject.myproject.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(HttpServletRequest request, HttpServletResponse response) {
-        return "index";
+    public String index(){
+        return "index.html";
     }
+    @ResponseBody
+    @GetMapping("hello")
+    public List<String> hello() {
+        return Arrays.asList("안녕하세요", "Hello");
+    }
+
 }
